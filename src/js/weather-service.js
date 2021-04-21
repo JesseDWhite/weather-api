9ad35cef9fcf3ac5d7a2768f4efc4f6fc8232345
Jesse White <jesse.white6@gmail.com>
2021-04-20 20:04:15 -0700

    add options for async and fetch methods.

--- src/js/weather-service.js
+++ src/js/weather-service.js
@@ -1,8 +1,9 @@
+//Promise and static method of obtaining the api
 export default class WeatherService {
     static getWeather(city) {
         return new Promise(function (resolve, reject) {
             let request = new XMLHttpRequest();
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
             request.onload = function () {
                 if (this.status === 200) {
                     resolve(request.response);
@@ -13,5 +14,35 @@ export default class WeatherService {
             request.open("GET", url, true);
             request.send();
         });
+    }
+}
+//Fetch method of obtaining the api call
+export default class WeatherService {
+    static getWeather(city) {
+        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`)
+            .then(function (response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .catch(function (error) {
+                return error;
+            })
+
+    }
+}
+//async and await functional javascript
+export default class WeatherService {
+    static async getWeather(city) {
+        try {
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        } catch (error) {
+            return error.message;
+        }
     }
 }
--- src/main.js
+++ src/main.js
@@ -15,23 +15,61 @@ function clearFields() {
     $('.showWindSpeed').text("");
     $('.showClouds').text("");
 }
+//code for fetch() method
+function getElements(response) {
+    if (response.main) {
+        $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    } else {
+        $('.showErrors').text(`There was an error: ${response.message}`);
+    }
+}
+//async and await functional javascript
+function getElements(response) {
+    if (response.main) {
+        $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    } else {
+        $('.showErrors').text(`There was an error: ${response}`);
+    }
+}
+async function makeApiCall(city) {
+    const response = await WeatherService.getWeather(city);
+    getElements(response);
+}
+
+async function makeApiCall(city) {
+    const response = await WeatherService.getWeather(city);
+    getElements(response);
+}
+
 
 $(document).ready(function () {
     $('#weatherLocation').click(function () {
         const city = $('#location').val();
         $('#location').val("");
         clearFields();
+        //async and await functional javascript
+        makeApiCall(city);
+        //async and await functional javascript
+
+        //code for fetch method
+        WeatherService.getWeather(city)
+            .then(function (response) {
+                getElements(response);
+            })
+        //code for fetch method
         let promise = WeatherService.getWeather(city);
 
         promise.then(function (response) {
             const body = JSON.parse(response);
-            $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-            $('.showTemp').text(`The temperature is ${Math.floor(response.main.temp)} degrees Fahrenheit.`);
-            $('.showDescription').text(`${Date(response.dt)}`);
-            $('.showLat').text(`Lat: ${response.coord.lat}.`);
-            $('.showLong').text(`Long: ${response.coord.lon}.`);
-            $('.showWindSpeed').text(`Wind Speed: ${response.wind.speed}.`);
-            $('.showClouds').text(`Clouds: ${response.clouds.all}%.`);
+            $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+            $('.showTemp').text(`The temperature is ${Math.floor(body.main.temp)} degrees Fahrenheit.`);
+            $('.showDescription').text(`${Date(body.dt)}`);
+            $('.showLat').text(`Lat: ${body.coord.lat}.`);
+            $('.showLong').text(`Long: ${body.coord.lon}.`);
+            $('.showWindSpeed').text(`Wind Speed: ${body.wind.speed}.`);
+            $('.showClouds').text(`Clouds: ${body.clouds.all}%.`);
         }, function (error) {
             $('.showErrors').text(`There was an error processing your request: ${error}`);
         });
